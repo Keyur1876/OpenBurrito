0cc6a66072b6bd62ad96905320957db2e21054b4
Erik Wenke <erik.wenke@stud.htw-dresden.de>
2026-01-08 14:08:55 +0100

    "/wiki" now routes to WikiView.vue

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import WikiView from '@/views/WikiView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -7,9 +8,14 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: HomeView
+      component: HomeView,
     },
-  ]
+    {
+      path: '/wiki',
+      name: 'wiki',
+      component: WikiView,
+    },
+  ],
 })
 
 export default router
