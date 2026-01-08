bb5b5f29d6865fa96211a6997961fb39db79de7b
Erik Wenke <erik.wenke@stud.htw-dresden.de>
2026-01-08 17:33:53 +0100

    add: store which holds wiki entires (for now) + enabled entries in wiki

---
+++ src/stores/wiki.js
@@ -0,0 +1,39 @@
+import { defineStore } from 'pinia'
+import { ref } from 'vue'
+
+export const useWikiStore = defineStore('wiki', () => {
+  const entries = ref([
+    {
+      id: 1,
+      name: 'Lokomotive Rock',
+      city: 'Kurort Rathen, Germany',
+      lat: 50.96658,
+      lng: 14.08351,
+      image: '/images/istockphoto1.jpg',
+      description:
+        'A distinctive sandstone formation near Kurort Rathen. Popular in the Saxon Switzerland area for traditional climbing and bouldering-style problems on solid sandstone.',
+    },
+    {
+      id: 2,
+      name: 'Rauenstein Plateau',
+      city: 'Kurort Rathen, Germany',
+      lat: 50.95181,
+      lng: 14.06185,
+      image: '/images/stockphoto2.jpg',
+      description:
+        'A large sandstone table mountain offering multiple rock formations and bouldering-style sections. Scenic views over the Elbe valley.',
+    },
+    {
+      id: 3,
+      name: 'Schwedenturm',
+      city: 'Kurort Rathen, Germany',
+      lat: 50.9605,
+      lng: 14.0738,
+      image: '/images/stockphoto3.jpg',
+      description:
+        'A narrow sandstone tower near the Schwedenlöcher area. Known for technical climbing and compact rock features typical of Saxon Switzerland.',
+    },
+  ])
+
+  return { entries }
+})
--- src/views/WikiView.vue
+++ src/views/WikiView.vue
@@ -1,7 +1,19 @@
 <template>
   <h1>Wiki</h1>
+  <DataView :value="wikiStore.entries">
+    <template #list="s">
+      <div v-for="(item, index) in s.items" :key="index">
+        {{ item }}
+      </div>
+    </template>
+  </DataView>
 </template>
 
-<script setup></script>
+<script setup>
+import DataView from 'primevue/dataview'
+import { useWikiStore } from '@/stores/wiki'
+
+const wikiStore = useWikiStore()
+</script>
 
 <style scoped></style>
