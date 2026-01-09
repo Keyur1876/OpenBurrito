3f99f48ee99f1578d3d21284358b2fc45920d306
Erik Wenke <erik.wenke@stud.htw-dresden.de>
2026-01-09 07:32:14 +0100

    filter as computation

--- src/views/WikiView.vue
+++ src/views/WikiView.vue
@@ -3,8 +3,9 @@
   <DataTable
     :value="wikiStore.entries"
     v-model:filters="filters"
-    filterDisplay="row"
     :globalFilterFields="['name', 'type']"
+    showHeaders="false"
+    size="large"
   >
     <template #empty> No wiki entries found. </template>
     <template #loading> Loading wiki entries. Please wait. </template>
@@ -17,18 +18,28 @@
       </IconField>
     </template>
     <Column field="name"> </Column>
-    <Column field="type" />
   </DataTable>
 </template>
 
 <script setup>
 import { useWikiStore } from '@/stores/wiki'
-import { DataTable, Column, InputText, IconField, InputIcon } from 'primevue'
+import { Card, DataTable, Column, InputText, IconField, InputIcon } from 'primevue'
 import WikiCard from '@/components/WikiCard.vue'
-import { ref } from 'vue'
+import { ref, computed } from 'vue'
 
 const wikiStore = useWikiStore()
 const filters = ref({ global: { value: null, matchMode: 'contains' } })
+const search = ref('')
+
+const filteredEntries = computed(() => {
+  if (!search.value) return wikiStore.entries
+
+  const q = search.value.toLowerCase()
+
+  return wikiStore.entries.filter(
+    (entry) => entry.name.toLowerCase().includes(q) || entry.type.toLowerCase().includes(q),
+  )
+})
 </script>
 
 <style scoped></style>
