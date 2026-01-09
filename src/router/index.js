5d2c19c41e3ea20b27b2982f29c89ceb81c9a418
Erik Wenke <erik.wenke@stud.htw-dresden.de>
2026-01-09 13:07:04 +0100

    set up router for live documentaion

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import WikiView from '@/views/WikiView.vue'
+import Doc from '@/components/Doc.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -17,8 +18,8 @@ const router = createRouter({
     },
     {
       path: '/doc',
-      name: 'documentation',
-      component: DocumentationPage,
+      name: 'doc',
+      component: Doc,
     },
   ],
 })
