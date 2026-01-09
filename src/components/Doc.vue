105438777bd6fe8fe789cd75041217db75c82758
Erik Wenke <erik.wenke@stud.htw-dresden.de>
2026-01-09 13:08:05 +0100

    vue component which renders documentation

---
+++ src/components/Doc.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <DoxenSidebar v-model="selectedDemo" :demos="demos" />
+    <VueDoxen v-model="selectedDemo" :demos="demos" />
+    <link
+      href="https://unpkg.com/highlightjs/styles/ir-black.css"
+      rel="stylesheet"
+      type="text/css"
+    />
+  </div>
+</template>
+
+<script>
+import { computed, ref } from 'vue'
+import { DoxenSidebar, VueDoxen } from 'vue-doxen'
+
+import 'vue-doxen/vue-doxen.css'
+
+import EditorView from '@/views/EditorView.vue'
+import MapView from '@/views/MapView.vue'
+
+export default {
+  name: 'DocumentationPage',
+  components: {
+    DoxenSidebar,
+    VueDoxen,
+  },
+  setup: function () {
+    const selectedDemo = ref('EditorView')
+
+    const demos = computed(() => {
+      return {
+        EditorView,
+        MapView,
+      }
+    })
+
+    return {
+      demos,
+      selectedDemo,
+    }
+  },
+}
+</script>
