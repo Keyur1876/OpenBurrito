e5188cb8637e8cd65662bf9048d04ab71f1b9431
Erik Wenke <erik.wenke@stud.htw-dresden.de>
2026-01-12 21:30:35 +0100

    refactor: reduce redundance

--- src/components/BottomBar.vue
+++ src/components/BottomBar.vue
@@ -3,13 +3,17 @@ import { useRouter } from 'vue-router'
 import { Button } from 'primevue'
 
 const router = useRouter()
+
+const buttons = [
+  { icon: 'pi pi-plus', route: '/add' },
+  { icon: 'pi pi-map', route: '/' },
+  { icon: 'pi pi-book', route: '/wiki' },
+  { icon: 'pi pi-user', route: '/' },
+]
 </script>
 
 <template>
   <nav class="flex justify-around items-center h-15 fixed bottom-0 z-1000 w-screen bg-white">
-    <Button icon="pi pi-plus" class="h-10 w-10" @click="router.push('/add')" />
-    <Button icon="pi pi-map" class="h-10 w-10" @click="router.push('/')" />
-    <Button icon="pi pi-book" class="h-10 w-10" @click="router.push('/wiki')" />
-    <Button icon="pi pi-user" class="h-10 w-10" @click="router.push('/')" />
+    <Button v-for="i in buttons" :icon="i.icon" class="h-10 w-10" @click="router.push(i.route)" />
   </nav>
 </template>
