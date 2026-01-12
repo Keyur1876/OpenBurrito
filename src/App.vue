7d8f316beccf64daa74f6f1425db285481fe8c40
Erik Wenke <erik.wenke@stud.htw-dresden.de>
2026-01-12 21:23:03 +0100

    refactored navination buttons

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,15 @@
 <script setup>
 import { useRouter, useRoute } from 'vue-router'
-import {BottomBar} from '@/components'
+import { BottomBar } from '@/components'
 import { Toast } from 'primevue'
 
 const router = useRouter()
 const route = useRoute()
 
-function handleGo(name) {
-  router.push({ name }) // assumes your routes have "name"
-}
 </script>
 
 <template>
   <RouterView />
 
-  <BottomBar @go="handleGo" v-if="route.name !== 'doc'" />
+  <BottomBar v-if="route.name !== 'doc'" />
 </template>
